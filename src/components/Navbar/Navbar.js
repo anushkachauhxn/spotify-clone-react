61d02aa0fcf065d392378d75ab353803346dd64b
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-09-19 11:45:34 +0530

    Dropdown Menu in Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -23,14 +23,36 @@ function Navbar({ home }) {
         <button className="upgrade-btn">
           <h4>Upgrade</h4>
         </button>
-        <button className="menu-btn">
-          <img
-            src="https://images.unsplash.com/photo-1593104547489-5cfb3839a3b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1153&q=80"
-            alt=""
-          />
-          <h4>Matilda_</h4>
-          <ion-icon name="caret-down"></ion-icon>
-        </button>
+        <div className="menu ">
+          <button
+            onClick={() => {
+              document.querySelector(".menu").classList.toggle("active");
+            }}
+            className="menu-btn"
+          >
+            <img
+              src="https://images.unsplash.com/photo-1593104547489-5cfb3839a3b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1153&q=80"
+              alt=""
+            />
+            <h4>Matilda_</h4>
+            <ion-icon name="caret-up"></ion-icon>
+            <ion-icon name="caret-down"></ion-icon>
+          </button>
+
+          <ul className="menu-list">
+            <li>
+              Account
+              <ion-icon name="open-outline"></ion-icon>
+            </li>
+            <li>Profile</li>
+            <li>
+              Upgrade to Premium
+              <ion-icon name="open-outline"></ion-icon>
+            </li>
+            <li>Settings</li>
+            <li className="logout">Log out</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
