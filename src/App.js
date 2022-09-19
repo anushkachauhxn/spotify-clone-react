f435fb210ef56587bb1a6b38c5630e7de25b37e4
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-09-19 20:16:14 +0530

    Responsive Style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Home from "./components/Home/Home";
 import Track from "./components/Track/Track";
 
 function App() {
+  const getWidth = () =>
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  let [width, setWidth] = useState(getWidth());
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      let temp = getWidth();
+      setWidth(temp);
+
+      console.log(temp, width);
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="main">
